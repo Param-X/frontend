2815ac8fba13de317b8627f262647abb2d1ca7bd
trapp <paramseh0000@gmail.com>
2025-02-10 23:53:17 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {WalletProvider} from '@suiet/wallet-kit';
-import '@suiet/wallet-kit/style.css';
+import { WalletProvider } from "@suiet/wallet-kit";
+import "@suiet/wallet-kit/style.css";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import TradeYields from "./pages/TradeYields.jsx";
 import Positions from "./pages/Positions.jsx";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/trade-yield",
-    element: <TradeYields />,
-  },
-  {
-    path: "/fixed-income",
-    element: <Positions />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <WalletProvider>
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/trade-yield" element={<TradeYields />} />
+          <Route path="/fixed-income" element={<Positions />} />
+        </Routes>
+      </HashRouter>
+    </React.StrictMode>
   </WalletProvider>
 );
